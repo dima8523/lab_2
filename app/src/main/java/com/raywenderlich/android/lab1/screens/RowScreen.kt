049bd5b6cc991ca3769b75d0fe3f10344c16aa48
Zver <dima.852.dma@gmail.com>
2023-03-09 22:17:05 +0300

    change RowScreen

--- app/src/main/java/com/raywenderlich/android/lab1/screens/RowScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/RowScreen.kt
@@ -1,19 +1,17 @@
-package com.raywenderlich.android.lab1.screens
-
-import android.text.Layout.Alignment
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
-import java.lang.reflect.Modifier
-
 
 val THREE_ELEMENT_LIST = listOf(R.string.first, R.string.second, R.string.third)
 @Composable
@@ -28,6 +26,16 @@ fun RowScreen(){
 
 @Composable
 fun MyRow(){
+    Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()){
 
-
+        THREE_ELEMENT_LIST.forEach{ textResId ->
+            Text(
+                text = stringResource(id = textResId),
+                fontSize = 18.sp
+            )
+        }
+    }
 }
+

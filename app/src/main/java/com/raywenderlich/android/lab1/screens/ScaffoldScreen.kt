e32752b9819229735501ec1cf3d9a41f1fc962ce
Zver <dima.852.dma@gmail.com>
2023-03-09 23:07:18 +0300

    change MyScaffold

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
@@ -1,10 +1,18 @@
 package com.raywenderlich.android.lab1.screens
 
+import MyColumn
+import MyRow
+import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.res.colorResource
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun ScaffoldScreen(){
@@ -16,7 +24,21 @@ fun ScaffoldScreen(){
 }
 
 @Composable
-fun MyScaffold(){
+fun MyScaffold() {
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val scope: CoroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        contentColor = colorResource(id = R.color.colorPrimary),
+        content = {
+            MyRow()
+        },
+
+        topBar = { MyTopAppBar(scaffoldState = scaffoldState) },
+        bottomBar = { MyBottomAppBar() },
+        drawerContent = { MyColumn() }
+    )
 
 }
 
